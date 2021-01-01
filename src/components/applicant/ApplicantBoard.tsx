6ba5619ff5ec3832a6599a8aa7f2f7db16441ed4
yyhan0915 <yyhan2059@hanmail.net>
2021-01-01 23:30:27 +0100

    eslint rule fixed

--- src/components/applicant/ApplicantBoard.tsx
+++ src/components/applicant/ApplicantBoard.tsx
@@ -54,7 +54,7 @@ const ApplicantBoard: React.FC<IProps> = ({ searchTerm }) => {
             setSnackBarMessage('Failed to get applicant list');
             setIsSnackBarOpen(true);
         }
-    }, []);
+    }, [applicantError]);
 
     const applicantsByStatus = [
         {
